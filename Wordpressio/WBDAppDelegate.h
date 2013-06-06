ba47af834a0562c9c1218078d456c5683edda04d
webdevotion <webdevotion>
2013-06-06 09:21:39 +0200

    Initial Commit

---
+++ Wordpressio/WBDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WBDAppDelegate.h
+//  Wordpressio
+//
+//  Created by Bram Plessers on 06/06/13.
+//  Copyright (c) 2013 Bram Plessers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Wordpressio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wordpressio
+//
+//  Created by Bram Plessers on 06/06/13.
+//  Copyright (c) 2013 Bram Plessers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WBDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WBDAppDelegate class]));
+    }
+}
